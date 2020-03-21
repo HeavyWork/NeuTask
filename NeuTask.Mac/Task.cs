5561395a2d65c8f224aa9f16bbd1e5cfeedcf67f
Il Harper <afanyiyu@hotmail.com>
2020-03-21 16:33:55 +0800

    feat: NeuTask.Mac Complete

--- NeuTask.Mac/Task.cs
+++ NeuTask.Mac/Task.cs
@@ -1,10 +1,195 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
 namespace NeuTask.Mac
 {
-    public class Task
+
+    /// <summary>
+    /// The main Task class.
+    /// </summary>
+    public abstract class Task : INotifyPropertyChanged, IDisposable
     {
-        public Task()
+
+        #region DataContext
+
+        /// <summary>
+        /// The Id of the task.
+        /// </summary>
+        public string Id { get; } = Guid.NewGuid().ToString();
+
+        /// <summary>
+        /// The name of the task.
+        /// </summary>
+        public string Name { get; } = "";
+
+        private string _target = "";
+
+        /// <summary>
+        /// The processing files of the task.
+        /// </summary>
+        public string Target
         {
+            get => _target;
+            set
+            {
+                _target = value;
+                OnPropertyChanged(nameof(Target));
+            }
         }
+
+        private TaskStatus _status = TaskStatus.Undefined;
+
+        public TaskStatus Status
+        {
+            get => _status;
+            set
+            {
+                _status = value;
+                OnPropertyChanged(nameof(Status));
+                OnPropertyChanged(nameof(DisplayStatus));
+            }
+        }
+
+        /// <summary>
+        /// The task status for display.
+        /// </summary>
+        public string DisplayStatus =>
+            TaskStatusConverter.Current.Convert(
+                _status,
+                typeof(string),
+                null,
+                CultureInfo.CurrentCulture) as string;
+
+        private string _message = "";
+
+        /// <summary>
+        /// Current message of the task.
+        /// </summary>
+        public string Message
+        {
+            get => _message;
+            set
+            {
+                _message = value;
+                OnPropertyChanged(nameof(Message));
+            }
+        }
+
+        private double _percentage;
+
+        /// <summary>
+        /// Current percentage of the task.
+        /// </summary>
+        public double Percentage
+        {
+            get => _percentage;
+            set
+            {
+                _percentage = value;
+                OnPropertyChanged(nameof(Percentage));
+            }
+        }
+
+        private bool _handled;
+
+        public bool Handled
+        {
+            get => _handled;
+            set
+            {
+                _handled = value;
+                OnPropertyChanged(nameof(Handled));
+            }
+        }
+
+        #endregion
+
+        #region Task Core
+
+        /// <summary>
+        /// Start the task.
+        /// </summary>
+        public abstract void Start();
+
+        /// <summary>
+        /// Stop the task.
+        /// </summary>
+        public abstract void Stop();
+
+        #endregion
+
+        #region PropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
+        #region Dispose
+
+        public void Dispose()
+        {
+            if (Status == TaskStatus.Running)
+            {
+                Stop();
+            }
+            GC.SuppressFinalize(this);
+        }
+
+        #endregion
+
+    }
+
+    /// <summary>
+    /// The task status enum.
+    /// </summary>
+    public enum TaskStatus
+    {
+        Undefined = 0,
+        Waiting = 1,
+        Running = 2,
+        Complete = 3,
+        Failed = 4
+    }
+
+    public class TaskStatusConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+            TaskStatusConvertDictionary.TryGetValue((TaskStatus)value, out string result);
+            return result;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is null) return null;
+            IEnumerable<KeyValuePair<TaskStatus, string>> result = TaskStatusConvertDictionary.Where(pair => pair.Value == value as string);
+            if (result.Any()) return result.FirstOrDefault().Key;
+            return null;
+        }
+
+        /// <summary>
+        /// The static TaskStatusConverter.
+        /// </summary>
+        public static TaskStatusConverter Current { get; } = new TaskStatusConverter();
+
+        public static Dictionary<TaskStatus, string> TaskStatusConvertDictionary { get; } =
+            new Dictionary<TaskStatus, string>()
+            {
+                {TaskStatus.Undefined, ""},
+                {TaskStatus.Waiting, "等待"},
+                {TaskStatus.Running, "运行"},
+                {TaskStatus.Complete, "完成"},
+                {TaskStatus.Failed, "失败"}
+            };
     }
 }
--- NeuTask.Mac/TaskManager.cs
+++ NeuTask.Mac/TaskManager.cs
@@ -1,10 +1,229 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
 namespace NeuTask.Mac
 {
-    public class TaskManager
+
+    /// <summary>
+    /// The main TaskManager class.
+    /// </summary>
+    public sealed class TaskManager : INotifyPropertyChanged
     {
+
         public TaskManager()
         {
+            TaskList.CollectionChanged += TaskListCollectionChanged;
         }
+
+        #region Current
+
+        /// <summary>
+        /// The current task manager.
+        /// </summary>
+        public static TaskManager Current { get; } = new TaskManager();
+
+        #endregion
+
+        #region TaskList Operators
+
+        /// <summary>
+        /// The task list.
+        /// </summary>
+        public ObservableCollection<Task> TaskList { get; set; } = new ObservableCollection<Task>();
+
+        /// <summary>
+        /// Add task to queue.
+        /// </summary>
+        /// <param name="task">The task.</param>
+        public void Push(Task task)
+        {
+            TaskList.Add(task);
+        }
+
+        /// <summary>
+        /// Remove task from queue.
+        /// </summary>
+        /// <param name="task">The task.</param>
+        public void Remove(Task task)
+        {
+            if (TaskList.Contains(task)) TaskList.Remove(task);
+        }
+
+        /// <summary>
+        /// Force stop the queue.
+        /// </summary>
+        public void Stop()
+        {
+            Queue = false;
+            CurrentTask?.Stop();
+        }
+
+        /// <summary>
+        /// Clear the queue. Do nothing when the current task is running.
+        /// </summary>
+        public void Clear()
+        {
+            if (Status == TaskStatus.Running) return;
+            _totalTask = 0;
+            TaskList.Clear();
+        }
+
+        #endregion
+
+        #region TaskManager Operator
+
+        private bool _queue;
+
+        /// <summary>
+        /// The queue dispatching status.
+        /// </summary>
+        public bool Queue
+        {
+            get => _queue;
+            set
+            {
+                if (!(CurrentTask is null))
+                {
+                    if (_queue && !value)
+                        CurrentTask.PropertyChanged -= QueueDispatch;
+                    else if (value && !_queue)
+                    {
+                        DispatchStart(true);
+                        CurrentTask.PropertyChanged += QueueDispatch;
+                    }
+                }
+                _queue = value;
+                OnPropertyChanged(nameof(Queue));
+            }
+        }
+
+        #endregion
+
+        #region TaskManager Status
+
+        public TaskStatus Status => CurrentTask?.Status ?? TaskStatus.Undefined;
+
+        public string DisplayStatus => CurrentTask?.DisplayStatus ?? "";
+
+        public string Message => CurrentTask?.Message ?? "";
+
+        public double TaskPercentage => CurrentTask?.Percentage ?? 0;
+
+        public double TotalPercentage => (TaskList.Count - 1) / (double)_totalTask;
+
+        #endregion
+
+        #region TotalPercentage Calculator
+
+        private int _totalTask;
+
+        #endregion
+
+        #region Queue Dispatcher
+
+        private void QueueDispatch(object sender, PropertyChangedEventArgs e)
+        {
+            if (e.PropertyName != nameof(CurrentTask.Status) || !TaskList.Contains(CurrentTask)) return;
+            if (
+                CurrentTask.Status == TaskStatus.Complete ||
+                CurrentTask.Status == TaskStatus.Failed && CurrentTask.Handled
+            ) TaskList.Remove(CurrentTask);
+        }
+
+        private void DispatchStart(bool flag = false)
+        {
+            if (!flag)
+            {
+                if (Queue && !(CurrentTask is null) && CurrentTask.Status == TaskStatus.Waiting) CurrentTask.Start();
+            }
+            else
+                if (!(CurrentTask is null) && CurrentTask.Status == TaskStatus.Waiting) CurrentTask.Start();
+        }
+
+        #endregion
+
+        #region CurrentTask Holder
+
+        private Task _currentTask;
+
+        /// <summary>
+        /// The current task: the first task in queue.
+        /// </summary>
+        public Task CurrentTask
+        {
+            get => _currentTask;
+            set
+            {
+                _currentTask = value;
+                OnPropertyChanged(nameof(CurrentTask));
+            }
+        }
+
+        #endregion
+
+        #region CurrentTask Event Handler
+
+        private void CurrentTaskOnPropertyChanged(object sender, PropertyChangedEventArgs e)
+        {
+            TaskPropertyReflectionDictionary.TryGetValue(e.PropertyName, out string value);
+            if (!string.IsNullOrEmpty(value)) OnPropertyChanged(value);
+        }
+
+        private static readonly Dictionary<string, string> TaskPropertyReflectionDictionary = new Dictionary<string, string>
+        {
+            { nameof(Task.Status), nameof(Status) },
+            { nameof(Task.DisplayStatus), nameof(DisplayStatus) },
+            { nameof(Task.Message), nameof(Message) },
+            { nameof(Task.Percentage), nameof(TaskPercentage) }
+        };
+
+        #endregion
+
+        #region TaskList Event Handler
+
+        private void TaskListCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (!TaskList.Any())
+            {
+                _totalTask = 0;
+                return;
+            }
+            if (e.Action == NotifyCollectionChangedAction.Add) _totalTask++;
+            if (CurrentTask != null && CurrentTask.Equals(TaskList[0])) return;
+            // Unplug Event Handler
+            if (CurrentTask != null)
+            {
+                if (Queue) CurrentTask.PropertyChanged -= QueueDispatch;
+                CurrentTask.PropertyChanged -= CurrentTaskOnPropertyChanged;
+                CurrentTask.Dispose();
+            }
+            // Replug Event Handler
+            CurrentTask = TaskList[0];
+            if (Queue) CurrentTask.PropertyChanged += QueueDispatch;
+            CurrentTask.PropertyChanged += CurrentTaskOnPropertyChanged;
+
+            DispatchStart();
+            OnPropertyChanged(nameof(TotalPercentage));
+        }
+
+        #endregion
+
+        #region PropertyChanged Handler
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+
     }
+
 }
